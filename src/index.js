c4bdbfa7b79dc34d5c5a3a7555277249b4f6dfad
mperdikeas <mperdikeas@gmail.com>
2016-09-12 22:39:58 +0300

    @npm version 0.0.6: only conditionally include the babel polyfill

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 //     http://stackoverflow.com/a/36628148/274677
 //
 
-require('babel-polyfill'); // this is important as Babel only transforms syntax (e.g. arrow functions)
+if (!global._babelPolyfill) // https://github.com/s-panferov/awesome-typescript-loader/issues/121
+    require('babel-polyfill'); // this is important as Babel only transforms syntax (e.g. arrow functions)
 // so you need this in order to support new globals or (in my experience) well-known Symbols, e.g. the following:
 //
 //     console.log(Object[Symbol.hasInstance]);
